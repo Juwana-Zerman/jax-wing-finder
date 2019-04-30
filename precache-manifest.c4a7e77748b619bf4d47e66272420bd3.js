ecfa2f76a5229c1ee70c6164e1ff8b946900b6da
Juwana Zerman <38802201+JZerman2018@users.noreply.github.com>
2019-04-30 15:07:11 -0400

    Updates

---
+++ precache-manifest.c4a7e77748b619bf4d47e66272420bd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24f6130ab20ab98a44417341b565079c",
+    "url": "/jax-wing-finder/index.html"
+  },
+  {
+    "revision": "7e6abcdcbcf63498c796",
+    "url": "/jax-wing-finder/static/css/main.ddba69bc.chunk.css"
+  },
+  {
+    "revision": "dfba66aebd9a3c431d3f",
+    "url": "/jax-wing-finder/static/js/2.98d77448.chunk.js"
+  },
+  {
+    "revision": "7e6abcdcbcf63498c796",
+    "url": "/jax-wing-finder/static/js/main.97bbfa4a.chunk.js"
+  },
+  {
+    "revision": "aca4985d947da897c86b",
+    "url": "/jax-wing-finder/static/js/runtime~main.b1018ee2.js"
+  }
+]);
