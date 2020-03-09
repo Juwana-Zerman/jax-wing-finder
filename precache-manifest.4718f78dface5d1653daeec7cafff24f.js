f35d2e6358e4d2ebd033432870bd0fda4afc77c0
Juwana Zerman <38802201+JZerman2018@users.noreply.github.com>
2020-03-09 14:34:24 -0400

    Updates

--- precache-manifest.4718f78dface5d1653daeec7cafff24f.js
+++ precache-manifest.4718f78dface5d1653daeec7cafff24f.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c782c15164410eb9552beb2bc4f45f41",
+    "revision": "f5733198baaf0afac39e3ab177428624",
     "url": "/jax-wing-finder/index.html"
   },
   {
-    "revision": "2b5b333591ad11fc9831",
-    "url": "/jax-wing-finder/static/css/main.7963a5a6.chunk.css"
+    "revision": "e7e9dead9bdebe7b039e",
+    "url": "/jax-wing-finder/static/css/main.36d3ab42.chunk.css"
   },
   {
     "revision": "2b94158c36071e7762fc",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/jax-wing-finder/static/js/2.72488f5c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2b5b333591ad11fc9831",
+    "revision": "e7e9dead9bdebe7b039e",
     "url": "/jax-wing-finder/static/js/main.d530eb7b.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jax-wing-finder/precache-manifest.032eb21675e61702f3033db813564784.js"
+  "/jax-wing-finder/precache-manifest.4718f78dface5d1653daeec7cafff24f.js"
 );
 
 self.addEventListener('message', (event) => {
